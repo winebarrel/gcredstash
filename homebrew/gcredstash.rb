387f3bfa7743d859e029e4aad8ed124c31413a45
winebarrel <sgwr_dts@yahoo.co.jp>
2016-06-08 21:14:20 +0900

    Update formula

--- homebrew/gcredstash.rb
+++ homebrew/gcredstash.rb
@@ -1,11 +1,11 @@
 require 'formula'
 
 class Gcredstash < Formula
-  VERSION = '0.1.0'
+  VERSION = '0.1.1'
 
   homepage 'https://github.com/winebarrel/gcredstash'
   url "https://github.com/winebarrel/gcredstash/releases/download/v#{VERSION}/gcredstash-v#{VERSION}-darwin-amd64.gz"
-  sha256 'e3601738293ed6141a0ee079a09f90ab65bfc5007eed68b25a5d88106cc01d82'
+  sha256 '4e5ecd149bcae3a4bbd94f0a85d32cd29d828143534aacf78c54df57c6a3843c'
   version VERSION
   head 'https://github.com/winebarrel/gcredstash.git', :branch => 'master'
 
