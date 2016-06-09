c101ac7f45846bc725d7cb9d38872acde3562f90
Genki Sugawara <sugawara@cookpad.com>
2016-06-09 10:18:43 +0900

    Fix sha256

--- homebrew/gcredstash.rb
+++ homebrew/gcredstash.rb
@@ -5,7 +5,7 @@ class Gcredstash < Formula
 
   homepage 'https://github.com/winebarrel/gcredstash'
   url "https://github.com/winebarrel/gcredstash/releases/download/v#{VERSION}/gcredstash-v#{VERSION}-darwin-amd64.gz"
-  sha256 '4e5ecd149bcae3a4bbd94f0a85d32cd29d828143534aacf78c54df57c6a3843c'
+  sha256 '7d8d303bc2b0182bf8e7a125e553533d20d075abe328c10809b809f6bf8d8ebc'
   version VERSION
   head 'https://github.com/winebarrel/gcredstash.git', :branch => 'master'
 
