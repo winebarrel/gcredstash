ab712790d6cb38a9dea883480e79716099a4abac
Genki Sugawara <sugawara@cookpad.com>
2016-06-09 10:17:35 +0900

    Bunp up version

--- homebrew/gcredstash.rb
+++ homebrew/gcredstash.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Gcredstash < Formula
-  VERSION = '0.1.1'
+  VERSION = '0.1.2'
 
   homepage 'https://github.com/winebarrel/gcredstash'
   url "https://github.com/winebarrel/gcredstash/releases/download/v#{VERSION}/gcredstash-v#{VERSION}-darwin-amd64.gz"
