75173d98bdfa43e9703d6b3511dea00dcf0b57b2
winebarrel <sgwr_dts@yahoo.co.jp>
2016-06-13 05:29:12 +0900

    Bump up version [ci skip]

--- gcredstash.spec
+++ gcredstash.spec
@@ -1,7 +1,7 @@
 %define  debug_package %{nil}
 
 Name:		gcredstash
-Version:	0.2.0
+Version:	0.2.1
 Release:	1%{?dist}
 Summary:	gcredstash manages credentials using AWS Key Management Service (KMS) and DynamoDB.
 
--- homebrew/gcredstash.rb
+++ homebrew/gcredstash.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Gcredstash < Formula
-  VERSION = '0.2.0'
+  VERSION = '0.2.1'
 
   homepage 'https://github.com/winebarrel/gcredstash'
   url "https://github.com/winebarrel/gcredstash/releases/download/v#{VERSION}/gcredstash-v#{VERSION}-darwin-amd64.gz"
