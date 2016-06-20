b249bf4df23db83cf09735624752cb3e6deb9da0
Genki Sugawara <sugawara@cookpad.com>
2016-06-20 15:18:35 +0900

    Bump up version

--- version.go
+++ version.go
@@ -1,7 +1,7 @@
 package main
 
 const Name string = "gcredstash"
-const Version string = "0.2.5"
+const Version string = "0.2.6"
 
 // GitCommit describes latest commit hash.
 // This value is extracted by git command when building.
