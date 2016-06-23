7c4d3bf4e4302c0db4a69925503e896023e03b7f
Genki Sugawara <sugawara@cookpad.com>
2016-06-23 17:44:28 +0900

    Fix formula sha256 [ci skip]

--- homebrew/gcredstash.rb
+++ homebrew/gcredstash.rb
@@ -5,7 +5,7 @@ class Gcredstash < Formula
 
   homepage 'https://github.com/winebarrel/gcredstash'
   url "https://github.com/winebarrel/gcredstash/releases/download/v#{VERSION}/gcredstash-v#{VERSION}-darwin-amd64.gz"
-  sha256 '20de9b6a5f674bb3bd248faae33916f320f8d25b88b7b72a1fb824d53177f0ba'
+  sha256 'c04c6de2c4464ce73e5c3c80aede33fd22a36df4c22b0b52c14d13add6a908f7'
   version VERSION
   head 'https://github.com/winebarrel/gcredstash.git', :branch => 'master'
 
