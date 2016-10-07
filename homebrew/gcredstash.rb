3afcceb61652f89573d9ca49247d8e3102cea308
Genki Sugawara <sugawara@cookpad.com>
2016-10-07 16:29:15 +0900

    Fix homebrew formula

--- homebrew/gcredstash.rb
+++ homebrew/gcredstash.rb
@@ -1,11 +1,11 @@
 require 'formula'
 
 class Gcredstash < Formula
-  VERSION = '0.2.8'
+  VERSION = '0.2.9'
 
   homepage 'https://github.com/winebarrel/gcredstash'
   url "https://github.com/winebarrel/gcredstash/releases/download/v#{VERSION}/gcredstash-v#{VERSION}-darwin-amd64.gz"
-  sha256 '83a6186072d85aa9efd4b384885b612a595a7573961babef981ec7d490572b64'
+  sha256 '8d5fa90ac6b0cd0fc8c927ff11b7ce340439e1e82aad7bb7e08fd145f51edacb'
   version VERSION
   head 'https://github.com/winebarrel/gcredstash.git', :branch => 'master'
 
