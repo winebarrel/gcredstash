bdf4f3fcf7090f83ca0fb0f05453b71227650d96
winebarrel <sgwr_dts@yahoo.co.jp>
2017-03-24 00:12:04 +0900

    Update homebrew check sum

--- homebrew/gcredstash.rb
+++ homebrew/gcredstash.rb
@@ -5,7 +5,7 @@ class Gcredstash < Formula
 
   homepage 'https://github.com/winebarrel/gcredstash'
   url "https://github.com/winebarrel/gcredstash/releases/download/v#{VERSION}/gcredstash-v#{VERSION}-darwin-amd64.gz"
-  sha256 '07314515872480d1d5a6f0590d58325efa1ab66476c2373c1e749597939d3327'
+  sha256 'fd46232581d11d922f9750e240f02405aee9508bfc1e13785895089f2c5f58dc'
   version VERSION
   head 'https://github.com/winebarrel/gcredstash.git', :branch => 'master'
 
