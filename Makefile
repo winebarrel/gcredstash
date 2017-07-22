16953373a56f3b4c5ad7bec5ab2b4ab59b6a79d9
Genki Sugawara <sugawara@cookpad.com>
2017-07-22 18:07:08 +0900

    Bump up version [ci skip]

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 SHELL:=/bin/bash
-VERSION=v0.3.3
+VERSION=v0.3.4
 GOOS=$(shell go env GOOS)
 GOARCH=$(shell go env GOARCH)
 RUNTIME_GOPATH=$(GOPATH):$(shell pwd)
--- homebrew/gcredstash.rb
+++ homebrew/gcredstash.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Gcredstash < Formula
-  VERSION = '0.3.3'
+  VERSION = '0.3.4'
 
   homepage 'https://github.com/winebarrel/gcredstash'
   url "https://github.com/winebarrel/gcredstash/releases/download/v#{VERSION}/gcredstash-v#{VERSION}-darwin-amd64.gz"
